e06a148506f4542885893eb7a6647639b1418837
bhyou <bhyou@foxmail.com>
2021-12-26 14:07:00 +0800

    add wishbone bfm

---
+++ proj/wishbone_bfm/bench/wshb_bfm_pkg.sv
@@ -0,0 +1,13 @@
+/*************************************************************************
+ > Copyright (C) 2021 Sangfor Ltd. All rights reserved.
+ > File Name   : wshb_bfm_pkg.sv
+ > Author      : bhyou
+ > Mail        : bhyou@foxmail.com 
+ > Created Time: Sun 26 Dec 2021 02:04:02 PM CST
+ ************************************************************************/
+ 
+package wshb_bfn_pkg;
+
+
+    
+endpackage
