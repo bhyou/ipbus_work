a158a73065419869059d98ea99c530e8e6288ed5
bhyou <bhyou@foxmail.com>
2021-12-27 21:07:53 +0800

    finish ipbus wishbone interface

--- proj/wishbone_bfm/bench/ipbus_wb_if.sv
+++ proj/wishbone_bfm/bench/ipbus_wb_if.sv
@@ -1,11 +1,12 @@
 /*************************************************************************
  > Copyright (C) 2021 Sangfor Ltd. All rights reserved.
- > File Name   : wshb_m_if.sv
+ > File Name   : ipbus_wb_if.sv
  > Author      : bhyou
  > Mail        : bhyou@foxmail.com 
  > Created Time: Sun 26 Dec 2021 01:50:54 PM CST
  ************************************************************************/
- 
+import ipbus_wb_pkg::*;
+
 interface ipb_if #(
     parameter DWIDTH=32,
     parameter AWIDTH=32
@@ -13,73 +14,87 @@ interface ipb_if #(
 
     parameter SELWIDTH = $clog2(DWIDTH);
 
-    logic  [DWIDTH-1:0]   wb_dat_i;
-    logic  [DWIDTH-1:0]   wb_dat_o;
-    logic  [AWIDTH-1:0]   wb_adr_o;
-    logic                 wb_cyc_o;
-    logic  [SELWIDTH-1:0] wb_sel_o;
-    logic                 wb_stb_o;
-    logic                 wb_we_o ;
-    logic                 wb_ack_i;
-    logic                 wb_err_i;
-    logic                 wb_rty_i;
+    logic  [DWIDTH-1:0]   rdata;
+    logic  [DWIDTH-1:0]   wdata;
+    logic  [AWIDTH-1:0]   addr;
+    logic                 strobe;
+    logic                 write;
+    logic                 ack;
+    logic                 error;
 
+    //master clocking block
     clocking mcb @(posedge clk);
         default input #1ns output #1ns;
-        output wb_dat_o;
-        output wb_adr_o;
-        output wb_cyc_o;
-        output wb_sel_o;
-        output wb_stb_o;
-        output wb_we_o;
-        input  wb_ack_i;
-        input  wb_err_i;
-        input  wb_rty_i;
-        input  wb_dat_i;
+        output   wdata;
+        output   addr;
+        output   strobe;
+        output   write;
+        input    ack;
+        input    rdata;
+        input    error; 
     endclocking
 
-    task reset();
+    // slave clocking block
+    clocking scb @(posedge clk);
+        default input #1ns output #1ns;
+        input   wdata;
+        input   addr;
+        input   strobe;
+        input   write;
+        output  ack;
+        output  rdata;
+        output  error; 
+    endclocking
+
+    modport master (
+        input clk, clocking mcb
+    );
+    modport slave (
+        input clk, clocking scb
+    );
+
+    task reset_operation();
+        mcb.wdata <= 0;
+        mcb.addr <= 0;
+        mcb.strobe <= 0;
+        mcb.write <= 0;
     endtask
 
-    task wb_bus_cycle();
-        mcb.wb_cyc_o <= 1'b1;
-        mcb.wb_stb_o <= 1'b1;
-        mcb.wb_adr_o <= addr;
-        mcb.wb_sel_o <= sel;
-        mcb.wb_dat_o <= data;
-        if(trns_type == WRITE) begin
-            mcb.wb_we_o <= 1'b1;
-        end else begin
-            mcb.wb_we_o <= 1'b0;
-        end
-
-        //wait for slave response
-
-        do begin
-            do begin
+    task write_operation(input bit32 addr, wrdata);
+        @mcb;
+        mcb.wdata <= wrdata;
+        mcb.addr <= addr;
+        mcb.strobe <= 1'b1;
+        mcb.write <= 1'b1;
+        fork
+           begin
+               #TIMEOUT;
+               $display("IPbus Wishbone interface timeout!");  
+           end
+           do begin
                 @mcb;
-                ack_cnt++;
-            end while((mcb.wb_ack_i !== 1'b1) && (mcb.wb_err_i !== 1'b1') &&
-                     (mcb.wb_rty_i !== 1'b1) && (ack_cnt != ack_timeout));
-            //check error signal
-            if(mcb.wb_err_i) begin
-                $error("transaction error detected at sim time %d",$time());
-            end
-
-            // check acknowledge timeout
-            if((mcb.wb_ack_i)&&(!mcb.wb_rty_i)) begin
-                $error("transaction timeout detected at sim time %d",$time());
-            end
-            retry_cnt++;
-        end while((mcb.wb_rty_i==1'b1)&&(retry_cnt!=max_retry));
-        //check retry signal
-        if(mcb.wb_rty_i) begin
-           $error("unexpected transaction Retey Detected at sim time %0t",$time()); 
-        end
-
-        //Get Data
-        out_data = mcb.wb_dat_i;
-        mcb.wb_cyc_o <= 1'b0;
+            end while((mcb.ack==1'b0) && (mcb.error==1'b0));
+        join_any
+        $display("Write data: 0x%8x to addr: %0d",wrdata, addr);
+        mcb.strobe <= 1'b0;
     endtask
 
+    task read_operation(input bit32 addr, output bit32 rddata);
+        @mcb;
+        mcb.addr <= addr;
+        mcb.strobe <= 1'b1;
+        mcb.write <= 1'b0;
+        fork
+           begin
+               #TIMEOUT;
+               $display("IPbus Wishbone interface timeout!");  
+           end
+           do begin
+                @mcb;
+                if(mcb.ack) rddata = rdata;
+            end while((mcb.ack==1'b0) && (mcb.error==1'b0));
+        join_any
+        $display("Read data: 0x%8x from addr: %0d",rddata, addr);
+        mcb.strobe <= 1'b0;
+    endtask
 endinterface
